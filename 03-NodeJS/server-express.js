09aba21a7915dd816b58b0fca639201cb76c36a9
Hugo LIEGEARD <hugo@hl-media.co>
2019-09-25 15:46:05 -0400

    Travail du 25/09 2/2
    - Mise en Place du Templating avec Nunjunks.

--- 03-NodeJS/server-express.js
+++ 03-NodeJS/server-express.js
@@ -12,19 +12,39 @@ const app = express();
 const port = 3000;
 
 /**
+ * Importer le package 'nunjucks'
+ * Configuration avec Express
+ * https://mozilla.github.io/nunjucks/getting-started.html
+ */
+const nunjucks = require('nunjucks');
+
+nunjucks.configure('views', {
+    autoescape: true,
+    express: app
+});
+
+/**
  * Les objets 'req' (requete) et 'res' (réponse)
  * sont exactement les mêmes que ceux fournit par Node.
  */
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/views/html/index.html');
+
+    let data = db.get('contacts')
+        .value();
+
+    console.log(data);
+    // res.sendFile(__dirname + '/views/html/index.html');
+    res.redirect('/contacts');
 });
 
 app.get('/contacts', (req, res) => {
-    res.sendFile(__dirname + '/views/html/contacts.html');
+    // res.sendFile(__dirname + '/views/html/contacts.html');
+    res.render('html/contacts.html');
 });
 
 app.get('/contact', (req, res) => {
-    res.sendFile(__dirname + '/views/html/contact.html');
+    // res.sendFile(__dirname + '/views/html/contact.html');
+    res.render('html/contact.html');
 });
 
 /**
