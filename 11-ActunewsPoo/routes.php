2813e922d0240ebc68266cd5e519a99ce6f707af
Hugo LIEGEARD <hugo@hl-media.co>
2019-11-15 15:38:28 -0400

    Automatisation des routes

--- 11-ActunewsPoo/routes.php
+++ 11-ActunewsPoo/routes.php
@@ -6,42 +6,6 @@ $controller = ucfirst( $_GET['controller'] ) . 'Controller'; // ?? 'DefaultContr
 $action     = $_GET['action']; // ?? 'home'; // home
 
 // -- ROUTAGE AUTOMATIQUE ------------------|
+call_user_func_array([ $controller, $action ], []);
 
-$obj = new $controller(); // DefaultController, AdminController, ...
-$obj->$action(); // ->home()
-
-// Approche Dynamique
-//$$controller = new $controller();
-//${$controller}->$action();
-
-// -- ROUTAGE MANUEL ------------------|
-
-// -- DEFAULT CONTROLLER
-# Maintenant que nous avons l'autoload, nous pouvons
-# commenter les require_once.
-# require_once 'src/Controller/DefaultController.php';
-# require_once 'src/Controller/MemberController.php';
-//$defaultCtrl = new DefaultController();
-//$memberCtrl  = new MemberController();
-
-//if ( $controller == 'default' && $action == 'home' ) {
-//    $defaultCtrl->home();
-//}
-//
-//if ( $controller == 'default' && $action == 'category' ) {
-//    $defaultCtrl->category();
-//}
-//
-//if ( $controller == 'default' && $action == 'article' ) {
-//    $defaultCtrl->article();
-//}
-
-// -- MEMBER CONTROLLER
-
-//if ( $controller == 'membre' && $action == 'register' ) {
-//    $memberCtrl->register();
-//}
-//
-//if ( $controller == 'membre' && $action == 'login' ) {
-//    $memberCtrl->login();
-//}
+// PRIVATE : return and Response / Request
